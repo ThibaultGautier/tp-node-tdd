66d7d783c91e5c5f0763c2f191ab5b5fc8a31998
Charles Duporge <charles.duporge@ynov.com>
2017-12-08 11:22:12 +0100

    Externalized course list routes in its own route file

--- app.js
+++ app.js
@@ -1,63 +1,35 @@
 const express = require("express")
 const app = express()
 const bodyparser = require("body-parser")
-const db = require("./test/fixtures/courses")
-const listeDeCourses = db.courseListe
+const courseListRouter = require("./routes/course-list-routes")
 
 app.use(bodyparser.json())
 
+app.use('/course', courseListRouter)
+
 if(!module.parent){
     app.listen(3000, function(){
-        console.log("Connecte");
+        console.log("Connected");
     })
 }
 
 
-app.route('/course')
-    .post(function(req, res){
-        db.push(req.body)
-        res.json(db);
-    })  
-    .get(function(req, res){
-        res.json(db)
-    })
-    .delete(function(req, res){
-        for(var i = 0; i<db.length; i++){
-            if(db[i].id == req.body.id){
-                db.splice(i, 1);
-            }
-        }
-        res.json(db)
-    })
 
-app.route('/course/:id/articles')
-    .get(function(req, res){
-        for(var i = 0; i<db.length; i++){
-            if(db[i].id == req.params.id){
-                res.json(db[0].articles)
-            }
-        }
-    })
+// app.use((req, res, next) => {
+//   return next(new NotFoundError())
+// })
 
-    .post(function(req, res){
-        for(var i = 0; i<db.length; i++){
-            if(db[i].id == req.params.id){
-                articlesList = db[0].articles
-                articlesList.push(req.body)
-                res.json(articlesList)
-            }
-        }
-    })
+// app.use((err, req, res, next) => {
+//   if (!(err instanceof HttpError)) {
+//     console.error(err)
+//     err = new HttpError(err.message || 'Unknown error')
+//   }
 
-app.route('/course/:course_id/articles/:article_id/flag')
-    .post(function(req, res){
-        for(var i = 0; i<db.length; i++){
-            if(db[i].id == req.params.course_id){
-                articlesList = db[0].articles
-                articlesList[req.params.article_id].flag = req.body.flag_value
-                res.json(articlesList)
-            }
-        }
-    })
+//   res.status(err.statusCode)
+//   res.json({
+//     error: err
+//   })
+// })
+
 module.exports = app
 
---
+++ routes/course-list-routes.js
@@ -0,0 +1,53 @@
+const express = require("express")
+const courseListRouter = express()
+const bodyparser = require("body-parser")
+const db = require("../test/fixtures/courses")
+
+courseListRouter.route('')
+    .post(function(req, res){
+        db.push(req.body)
+        res.json(db);
+    })  
+    .get(function(req, res){
+        res.json(db)
+    })
+    .delete(function(req, res){
+        for(var i = 0; i<db.length; i++){
+            if(db[i].id == req.body.id){
+                db.splice(i, 1);
+            }
+        }
+        res.json(db)
+    })
+
+courseListRouter.route('/:id/articles')
+    .get(function(req, res){
+        for(var i = 0; i<db.length; i++){
+            if(db[i].id == req.params.id){
+                res.json(db[0].articles)
+            }
+        }
+    })
+
+    .post(function(req, res){
+        for(var i = 0; i<db.length; i++){
+            if(db[i].id == req.params.id){
+                articlesList = db[0].articles
+                articlesList.push(req.body)
+                res.json(articlesList)
+            }
+        }
+    })
+
+courseListRouter.route('/:course_id/articles/:article_id/flag')
+    .post(function(req, res){
+        for(var i = 0; i<db.length; i++){
+            if(db[i].id == req.params.course_id){
+                articlesList = db[0].articles
+                articlesList[req.params.article_id].flag = req.body.flag_value
+                res.json(articlesList)
+            }
+        }
+    })
+
+module.exports = courseListRouter
